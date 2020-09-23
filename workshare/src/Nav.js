037ec94203de3a6aa249efc8a5b5328aae2f17b4
unknown <utkarshsudhir.181it250@nitk.edu.in>
2020-09-23 11:12:50 +0530

    Updated

--- workshare/src/Nav.js
+++ workshare/src/Nav.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 class Nav extends React.Component {
@@ -30,37 +30,5 @@ class Nav extends React.Component {
 		);
 	}
 } 	
-=======
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-class Nav extends React.Component {
-	constructor(props){
-		super(props);
-	}
-	onLogin = () =>{
-		this.props.onRouteChange('signin');
-	}
-	onSignUp = () =>{
-		this.props.onRouteChange('register');
-	}
-	render(){
-		return(
-		<div>
-			<div className="card text-center">
-			  <div className="card-header">
-			    <ul className="nav nav-pills card-header-pills float-right">
-			      <li className="nav-item pr-3">
-			        <button type="button" className="btn btn-outline-success" onClick = {this.onLogin}>Login</button>
-			      </li>
-			      <li className="nav-item">
-			        <button type="button" className="btn btn-outline-success" onClick = {this.onSignUp}>SignUp</button>
-			      </li>
-			    </ul>
-			  </div>
-			</div>
-		</div>
-		);
-	}
-} 	
->>>>>>> 07de71fe5269f6c517cb3b5ee124676d42a5b078
+
 export default Nav;
--- workshare/src/signin.js
+++ workshare/src/signin.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 class signin extends Component {
@@ -30,7 +30,7 @@ class signin extends Component {
 		.then(data => {
 			console.log(data);
 			if (data.id){
-				this.props.onRouteChange('/');
+				this.props.onRouteChange('home');
 			}
 			else{
 				alert('Login or Password incorrect.');
@@ -61,68 +61,5 @@ class signin extends Component {
 			);
 	}
 }
-=======
-import React, {Component} from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-class signin extends Component {
-	constructor (props){
-		super(props);
-		this.state = {
-			signinField: '',
-			signinPass: ''
-		}
-	}
-	onFieldChange = (event) =>{
-		this.setState({ signinField: event.target.value})
-	}
-	onPasswordChange = (event) => {
-		this.setState({signinPass: event.target.value});
-	}
-	onSubmitSignIn = () => {
-		console.log(this.state.signinField);
-		console.log(this.state.signinPass);
-		fetch('http://localhost:3000/signin',{
-			method: 'post',
-			headers: {'Content-Type': 'application/json'},
-			body: JSON.stringify({
-				email: this.state.signinField,
-				password: this.state.signinPass
-			})
-		})
-		.then(response => response.json())
-		.then(data => {
-			console.log(data);
-			if (data.id){
-				this.props.onRouteChange('/');
-			}
-			else{
-				alert('Login or Password incorrect.');
-			}
-		})
 
-	}
-	render(){
-		return (
-		<div style={{ JustifyContent: 'center'}}>
-		<form>
-			  <div className="form-group">
-			    <label >Email address</label>
-			    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange = {this.onFieldChange}/>
-			    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-			  </div>
-			  <div className="form-group">
-			    <label >Password</label>
-			    <input type="password" className="form-control" id="exampleInputPassword1" onChange = {this.onPasswordChange}/>
-			  </div>
-			  <div className="form-group form-check">
-			    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-			    <label className="form-check-label" >Check me out</label>
-			  </div>
-			  <button type="submit" className="btn btn-primary" onClick = {this.onSubmitSignIn} >Submit</button>
-			</form>
-		</div>
-			);
-	}
-}
->>>>>>> 07de71fe5269f6c517cb3b5ee124676d42a5b078
 export default signin;
